59f75c6567ef95321bedfb482d17ab698b0563b1
Cecilist <jcecil239@gmail.com>
2022-06-08 16:51:04 -0400

    Groundwork for adding a card to the board, with testing.

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import card
 
+board = [0,0,0,0,0,0,0,0,0]
+c1 = card.Card("Test1",1,1,1,1,"Red")
+c2 = card.Card("Test2",1,1,1,1,"Red")
+c3 = card.Card("Test3",1,1,1,1,"Red")
+c4 = card.Card("Test4",1,1,1,1,"Red")
+c5 = card.Card("Test5",1,1,1,1,"Red")
+c6 = card.Card("Test6",1,1,1,1,"Red")
+c7 = card.Card("Test7",1,1,1,1,"Red")
+c8 = card.Card("Test8",1,1,1,1,"Red")
+c9 = card.Card("Test9",1,1,1,1,"Red")
+c10 = card.Card("Test10",1,1,1,1,"Red")
+
+
+
+def playCard(pos, c):
+    playedCard = c
+    board[pos] = playedCard
+    playedCard.setPos(pos)
+    return board
+    
+
 def main():
 
-    c1 = card.Card("Test",2,2,2,2,"Red")
-    
-    print(c1)
+    print(board)
+    # playCard(3, c2)    
+    print(board)   
+    print(board[3])
+    # print(c1)
     return 
 
 
--- test_card.py
+++ test_card.py
@@ -1,11 +1,16 @@
 import pytest
 import card
+import main
 
 
     
 @pytest.fixture
 def dummyCard():
     return card.Card("test", 1, 2, 3, 4, "Blue")
+
+# @pytest.fixture
+# def board():
+#     return [0,0,0,0,0,0,0,0,0]
 
 def test_card_ini(dummyCard):
     print("testing initialization of a card")
@@ -15,4 +20,8 @@ def test_card_setPos(dummyCard):
     print("testing the set position function of a card")
     dummyCard.setPos(5)
     assert (5) == (dummyCard.pos)
-        
+
+def test_play_card(dummyCard):
+    print("testing that the playCard funtion adds a card to the board in correct position")
+    assert ([0,0,0,0,dummyCard,0,0,0,0]) == main.playCard(4, dummyCard)
+
